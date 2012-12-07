417c5c9035b1a5ca949db5d906aa846b9165cb28
Michael Pankov <constantius9@gmail.com>
2012-12-07 21:20:42 +0400

    Сделал тест для генерации команд

--- course11/scripts/run_single_program.py
+++ course11/scripts/run_single_program.py
@@ -8,68 +8,6 @@ import textwrap as tw
 import collections as cl
 
 
-def build_reference(settings):
-    """Build the reference version of the benchmark."""
-    local_settings = create_local_settings(settings)
-    command = tw.dedent("""
-        {compiler} -O0 -I utilities -I {benchmark_source_dir} 
-        utilities/polybench.c {benchmark_source_dir} 
-        -DPOLYBENCH_DUMP_ARRAYS 
-        -o ./bin/{program_name}_ref""").translate(None, '\n').format(
-        **local_settings)
-    os.chdir(local_settings['benchmark_root_dir'])
-    print os.path.realpath(os.path.curdir)
-    print command
-    raw_input()
-    reference_command_build = tw.dedent("""\
-        gcc -O0 -I utilities -I linear-algebra/kernels/atax 
-        utilities/polybench.c linear-algebra/kernels/atax/atax.c 
-        -DPOLYBENCH_DUMP_ARRAYS -o ./bin/atax_ref""").translate(None, '\n')
-    assert command == reference_command_build
-    sp.call('mkdir bin'.split())
-    sp.check_call(command.split())
-
-
-def run_reference(settings):
-    reference_command_run = './atax_ref 2>atax_ref.out'
-
-
-def prepare_command(settings):
-    command = tw.dedent("""
-        {compiler} {base_opt} -I utilities 
-        -I {benchmark_source_dir} utilities/polybench.c 
-        {benchmark_source_dir}/{program_source} -DPOLYBENCH_TIME 
-        -o ./bin/{program_name}_time""").translate(None, '\n').format(
-        **local_settings)
-    return command
-
-
-def create_local_settings(settings):
-    """Create local settings from global."""
-    local_settings = dict()
-    local_settings['program_source'] = '{program_name}.c'.format(
-        **{'program_name': settings.program_name})
-    local_settings.update(settings._asdict())
-    return local_settings
-
-
-def build_timed(settings):
-    """Build the timed version of the benchmark."""
-    local_settings = create_local_settings(settings)
-    command = prepare_command(local_settings)
-    os.chdir(local_settings['benchmark_root_dir'])
-    print os.path.realpath(os.path.curdir)
-    print command
-    raw_input()
-    reference_command_build == tw.dedent("""\
-        gcc -O3 -I utilities -I linear-algebra/kernels/atax 
-        utilities/polybench.c linear-algebra/kernels/atax/atax.c 
-        -DPOLYBENCH_TIME -o ./bin/atax_time""").translate(None, '\n')
-    assert command == reference_command_build
-    sp.call('mkdir bin'.split())
-    sp.check_call(command.split())
-
-
 def main():
     """Invokes all necessary builds and experiments."""
     Settings = cl.namedtuple('Settings', 
@@ -82,6 +20,63 @@ def main():
 
     build_reference(settings)
     build_timed(settings)
+
+
+def create_local_settings(settings):
+    """Create local settings from global."""
+    local_settings = dict()
+    local_settings['program_source'] = '{program_name}.c'.format(
+        **{'program_name': settings.program_name})
+    local_settings.update(settings._asdict())
+    return local_settings
+
+
+def prepare_command_reference(settings):
+    command = tw.dedent("""
+        {compiler} -O0 -I utilities -I {benchmark_source_dir} 
+        utilities/polybench.c {benchmark_source_dir}/{program_source} 
+        -DPOLYBENCH_DUMP_ARRAYS 
+        -o ./bin/{program_name}_ref""").translate(None, '\n').format(
+        **settings)
+    return command
+
+
+def prepare_command_timed(settings):
+    command = tw.dedent("""
+        {compiler} {base_opt} -I utilities 
+        -I {benchmark_source_dir} utilities/polybench.c 
+        {benchmark_source_dir}/{program_source} -DPOLYBENCH_TIME 
+        -o ./bin/{program_name}_time""").translate(None, '\n').format(
+        **settings)
+    return command
+
+
+def build_reference(settings):
+    """Build the reference version of the benchmark."""
+    local_settings = create_local_settings(settings)
+    command = prepare_command_reference(local_settings)
+    os.chdir(local_settings['benchmark_root_dir'])
+    print os.path.realpath(os.path.curdir)
+    print command
+    raw_input()
+    sp.call('mkdir bin'.split())
+    sp.check_call(command.split())
+
+
+def run_reference(settings):
+    reference_command_run = './atax_ref 2>atax_ref.out'
+
+
+def build_timed(settings):
+    """Build the timed version of the benchmark."""
+    local_settings = create_local_settings(settings)
+    command = prepare_command_timed(local_settings)
+    os.chdir(local_settings['benchmark_root_dir'])
+    print os.path.realpath(os.path.curdir)
+    print command
+    raw_input()
+    sp.call('mkdir bin'.split())
+    sp.check_call(command.split())
 
 
 if __name__ == '__main__':
--- course11/scripts/test_run_single_program.py
+++ course11/scripts/test_run_single_program.py
@@ -2,10 +2,40 @@
 
 """Test the script for running single experiment."""
 
-import unittest as ut
+import unittest
 import run_single_program as testee # This is like a 'callee' to a 'caller'
+import collections
+import textwrap
 
+
+class TestBuildCommandsPreparation(unittest.TestCase):
+    def setUp(self):
+        Settings = collections.namedtuple('Settings', 
+            'compiler base_opt program_name '
+            'benchmark_root_dir benchmark_source_dir')
+        settings = Settings(compiler='gcc', base_opt='-O3', 
+            program_name='atax', 
+            benchmark_root_dir='../data/sources/polybench-c-3.2',
+            benchmark_source_dir='linear-algebra/kernels/atax')
+        self.local_settings = testee.create_local_settings(settings)
+    
+    def test_prepare_command_reference(self):
+        command = testee.prepare_command_reference(self.local_settings)
+        command_reference = textwrap.dedent("""\
+            gcc -O0 -I utilities -I linear-algebra/kernels/atax 
+            utilities/polybench.c linear-algebra/kernels/atax/atax.c 
+            -DPOLYBENCH_DUMP_ARRAYS -o ./bin/atax_ref""").translate(None, '\n')
+        self.assertEquals(command, command_reference)
+
+
+    def test_prepare_command_timed(self):
+        command = testee.prepare_command_timed(self.local_settings)
+        command_reference = textwrap.dedent("""\
+            gcc -O3 -I utilities -I linear-algebra/kernels/atax 
+            utilities/polybench.c linear-algebra/kernels/atax/atax.c 
+            -DPOLYBENCH_TIME -o ./bin/atax_time""").translate(None, '\n')
+        self.assertEquals(command, command_reference)
 
 
 if __name__ == '__main__':
-    ut.main()
+    unittest.main()
