2fcc9ea522fcc6ab3dc8c2a547f9c7eaa9f93e79
Michael Pankov <constantius9@gmail.com>
2012-12-22 22:21:17 +0400

    Добавил калибровку. Добавил валидацию результата измерения. Добавил поддержку стека путей. Добавил калибровочные исходники (пустая программа и usleep(0)).

--- course11/impl/src/run_single_program.py
+++ course11/impl/src/run_single_program.py
@@ -2,6 +2,7 @@
 
 """Run experiment with specified program with given compilers."""
 
+import sys
 import os
 import subprocess as sp
 import textwrap as tw
@@ -10,6 +11,8 @@ import couchdbkit as ck
 from couchdbkit.designer import push
 import recordtype as rt
 import collections as cl
+import timeit
+import numpy as np
 
 
 class Experiment(ck.Document):
@@ -20,32 +23,242 @@ class Experiment(ck.Document):
     datetime = ck.DateTimeProperty()
 
 
+class PrintableStructure():
+    def __str__(self):
+        c = self.__class__
+        print c
+        s = tw.dedent("""
+        {name}:
+        """).format(name=c.__name__)
+        for k, v in self._asdict().items():
+            s += '\t{field:20}:\t{value}\n'.format(field=k, value=v)
+
+        return s
+
+
 Input = cl.namedtuple('Input',
     'benchmark_source_dir compiler base_opt')
+
+
+CalibrationResultBase = cl.namedtuple('CalibrationResult',
+    'total_time time dispersion variance runs_number times_list')
+
+class CalibrationResult(PrintableStructure, CalibrationResultBase):
+    pass
+
+
+ValidationResultBase = cl.namedtuple('ValidationResult',
+    'real_time measured_time error relative_error')
+
+class ValidationResult(PrintableStructure, ValidationResultBase):
+    pass
+
+
 Settings = rt.recordtype('Settings', 
     'program_name benchmark_root_dir framework_root_dir')
+
 BuildSettings = rt.recordtype('BuildSettings',
     'compiler base_opt optimization_flags other_flags '
     'benchmark_source_dir')
+
 RunSettings = rt.recordtype('RunSettings',
     'benchmark_bin_dir '
     'handler_stdout handler_stderr')
 
 
+class NonAbsolutePathError(RuntimeError):
+    pass
+
+class NoSuchNestedPathError(RuntimeError):
+    pass
+
+
+definition = \
+"""
+from subprocess import Popen, PIPE
+def run():
+    p = Popen("{command}".split(), 
+              stdout=PIPE, 
+              stderr=PIPE)
+    return p.communicate()
+"""
+
+
 def main():
     """Invoke all necessary builds and experiments."""
-    settings = Settings(compiler='gcc', base_opt='-O3', 
-        program_name='atax', 
-        benchmark_root_dir='../data/sources/polybench-c-3.2',
-        benchmark_source_dir='linear-algebra/kernels/atax',
-        framework_root_dir=os.path.realpath('..'))
-    settings.benchmark_root_dir = os.path.realpath(
-        settings.benchmark_root_dir)
+    settings = Settings(program_name='atax', 
+        framework_root_dir=os.path.join(os.path.dirname(__file__), '..')
+        benchmark_root_dir=None,
+        benchmark_bin_dir=None)
+    settings.benchmark_root_dir = os.path.realpath(os.path.join(
+        settings.framework_root_dir, '/data/sources/polybench-c-3.2/'))
+    settings.benchmark_bin_dir = os.path.realpath(os.path.join(
+        settings.framework_root_dir, '/data/bin/'))
+    settings.paths_stack = []
+    nest_path_absolute(settings, settings.framework_root_dir)
+
     server, db = setup_database(settings)
 
     perform_experiment()
 
-    print_experiments(db)
+    unnest_path(settings)
+    assert len(settings.paths_stack) == 0
+
+
+def push_path(settings, path):
+    """
+    Push path to stack in settings.
+
+    Path must be absolute.
+    """
+    if os.path.isabs(path):
+        settings.paths_stack.append(path)
+    else:
+        raise NonAbsolutePathError
+
+
+def pop_path(settings):
+    """
+    Pop path from stack in settings.
+
+    Path is absolute.
+    """
+    return settings.paths_stack.pop()
+
+
+def get_path(settings):
+    """
+    Return the path on top of stack in settings.
+    """
+    return settings.paths_stack[-1]
+
+
+def ensure_path(settings):
+    """
+    Get the correct current path from stack in settings and 
+    change current directory to there.
+    """
+    os.path.chdir(get_path(settings))
+
+
+def nest_path_absolute(settings, path):
+    """
+    Receive path, push the real path of it to stack in settings and 
+    change current directory to there.
+    """
+    try:
+        os.chdir(path)
+    except:
+        raise NoSuchNestedPathError
+
+    push_path(settings, path)
+    ensure_path(settings)
+
+
+def nest_path_from_root(settings, path):
+    """
+    Receive path, relative to the root of framework, 
+    push it to stack in settings and change current directory to there.
+    """
+    new_path = os.path.join(settings.framework_root_dir, path)
+    nest_path_absolute(settings, new_path)
+
+
+def nest_path_from_benchmark_root(settings, path):
+    """
+    Receive path, relative to the root of benchmark directory, 
+    push it to stack in settings and change current directory to there.
+    """
+    new_path = os.path.join(settings.benchmark_root_dir, path)
+    nest_path_absolute(settings, new_path)
+
+
+def nest_path(settings, path):
+    """
+    Receive relative path, push the real path of it to stack in settings and 
+    change current directory to there.
+    """
+    new_path = os.path.join(get_path(settings), path)
+    nest_path_absolute(settings, new_path)
+
+
+def unnest_path(settings):
+    """
+    Pop the path from stack in settings and
+    change current directory to current top path of stack.
+    """
+    pop_path(settings)
+    ensure_path(settings)
+
+
+def validate(settings):
+    """
+    Perform a calibrated measurement of execution time and 
+    calculate the error.
+    """
+    nest_path_from_root(settings, '/data/bin/')
+
+    real_time_us = 0
+    overhead_time = validate_single('do_nothing', real_time_us, 0)
+
+    real_time_us = 500000
+    validate_single('atax_base', real_time_us, overhead_time)
+    unnest_path(settings)
+
+
+def validate_default(settings):
+    """
+    Perform validation on set of time-measurement programs and report errors.
+    """
+    nest_path_from_root(settings, '/data/bin/')
+    
+    for i in range(7):
+        real_time_us = 10**i
+        s = 'usleep_{0}'.format(real_time_us)
+        validate_single(s, real_time_us / 10**6, overhead_time)
+    unnest_path(settings)
+
+
+def validate_single(s, real_time, overhead_time):
+    result = calibrate(s)
+    measured_time = result.time - overhead_time
+    error = abs(measured_time - real_time)
+    relative_error = error / measured_time
+    print ValidationResult(real_time, measured_time, error, relative_error)
+    return real_time
+
+
+def make_running_function(command):
+    """Make a function which runs the specified command."""
+    def run():
+        p = sp.Popen(command.split(), 
+                     stdout=sp.PIPE, 
+                     stderr=sp.PIPE)
+        return p.communicate()
+    return run
+
+
+def calibrate(command):
+    """Calibrate execution of command until measurement is accurate enough."""
+    n = 0
+    t = 0
+    d_rel = 1
+    print "Begin"
+    while (t < 1) and (d_rel > 0.05):
+        sys.stderr.write('.')
+        n += 1
+        number = 10**(n)
+        result = timeit.repeat(stmt='run()', 
+                               setup=definition.format(command=command), 
+                               number=number,
+                               repeat=3)
+        # t = np.average(np.array(result))
+        t = min(result)
+        d = np.std(np.array(result))
+        d_rel = d / t
+        # print t, t / number, d, d_rel, n, result
+    sys.stderr.write('\n')
+    return CalibrationResult(t, t / number, d, d_rel, number, result)
 
 
 def convert_input_to_settings(input):
@@ -60,7 +273,9 @@ def convert_input_to_settings(input):
 
 def perform_experiment(iterations=None):
     """Perform experiment with given number of iterations."""
-    iterations = 1 if iterations is None
+    if iterations is None:
+        iterations = 1
+
     build_reference(settings)
     build_timed(settings)
 
@@ -143,7 +358,8 @@ def build_reference(settings):
     """Build the reference version of the benchmark."""
     local_settings = create_local_settings(settings)
     command = prepare_command_build_reference(local_settings)
-    os.chdir(local_settings['benchmark_root_dir'])
+    bin_dir = os.path.join(local_settings['framework_root_dir'], '/data/')
+    os.chdir(bin_dir)
     print os.path.realpath(os.path.curdir)
     print command
     sp.call('mkdir bin'.split())
@@ -154,7 +370,8 @@ def build_timed(settings):
     """Build the timed version of the benchmark."""
     local_settings = create_local_settings(settings)
     command = prepare_command_build_timed(local_settings)
-    os.chdir(local_settings['benchmark_root_dir'])
+    bin_dir = os.path.join(local_settings['framework_root_dir'], '/data/')
+    os.chdir(bin_dir)
     print os.path.realpath(os.path.curdir)
     print command
     sp.call('mkdir bin'.split())
